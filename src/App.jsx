e195e277ec8f8bab20aa93b97ee8e37c34d2545a
GabeeeM <gabrielmack210@gmail.com>
2022-11-22 10:14:59 -0800

    testing netlify thing

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is a test to see the netlify thing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
