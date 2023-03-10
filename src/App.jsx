705b8c3148ae606fab4e7d4af56bb6c13cdd0c03
Gabriel Mack <gabrielmack210@gmail.com>
2023-03-10 10:22:15 -0800

    getting ide set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import awesome from './awesome.jpg';
-import './App.css';
+import awesome from "./awesome.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={awesome} className="App-logo" alt="logo" />
-        <p>
+        <p className="font-bold text-[5rem]">
           Hello everyone this is an awesome test of my site
         </p>
       </header>
